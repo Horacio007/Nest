fec33d82804a94985307cee0da4155c38a30df73
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-23 13:13:27 -0600

    se termina 78 responder a un error en especifico

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -15,9 +15,16 @@ export class PokemonService {
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    const pokemon = await this.pokemonModel.create(createPokemonDto);
 
-    return pokemon;
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000) throw new BadRequestException(`Pokemon already exists in db ${JSON.stringify(error.keyValue)}`);
+
+      console.log(error);
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+    }
   }
 
   findAll() {
