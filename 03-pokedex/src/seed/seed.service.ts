17c54d01d42880002953c37090ce1f6ad599e4f6
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-24 08:53:24 -0600

    se termina 92 realizar peticiones http desde nest

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interfaces';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `seed executed`;
+  private readonly axios:AxiosInstance = axios;
+
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=10`);
+
+    data.results.forEach(({name,url}) => {
+      const segments:string[] = url.split('/');
+      const no:number = +segments[segments.length-2];
+    })
+
+    return data.results;
   }
 
 }
