da3a0132879e2a4dde991a8c6f56e51100a841ca
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-24 09:37:09 -0600

    se termina 93 y 94 tarea de unsercion de una api a la base, de una forma

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interfaces';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { CreatePokemonDto } from 'src/pokemon/dto';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios:AxiosInstance = axios;
 
+  constructor(private readonly pokemonService:PokemonService) { }
+
   async executeSeed() {
     const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=10`);
 
-    data.results.forEach(({name,url}) => {
+    data.results.forEach(async({name,url}) => {
       const segments:string[] = url.split('/');
       const no:number = +segments[segments.length-2];
+
+      let pokemon = new CreatePokemonDto();
+      pokemon = {no, name};
+      
+      await this.pokemonService.create(pokemon);
     })
 
-    return data.results;
+    return 'Seed executed';
   }
 
 }
