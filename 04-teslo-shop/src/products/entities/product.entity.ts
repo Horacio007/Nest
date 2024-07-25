211860d56a561970e369fe0a9bdf73cb40bebeab
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-25 09:43:55 -0600

    se termina 128 create productdto

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -10,4 +10,40 @@ export class Product {
         length:50
     })
     title:string;
+
+    @Column('decimal',{
+        scale:4,
+        precision:16
+    })
+    price:number;
+
+    @Column('varchar',{
+        length:100,
+        nullable: true
+    })
+    description:string;
+
+    @Column('varchar',{
+        length:50,
+        unique: true
+    })
+    slug:string;
+
+    @Column('int',{
+        default:0
+    })
+    stock:number;
+
+    @Column('simple-array',{
+        select: false
+    })
+    sizes:string[];
+
+    @Column('varchar',{
+        length:10
+    })
+    gender:string;
+    
+    // tags
+    // images
 }
