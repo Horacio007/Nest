f7909a2a528921c91c3b990ff7d70473c7241078
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-12 11:22:12 -0600

    se termina 162 servir archivos de manera controlada

--- 04-teslo-shop-postgrase/src/files/files.controller.ts
+++ 04-teslo-shop-postgrase/src/files/files.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ErrorHandleService } from 'src/common/common.error-handle.service';
 import { TypeError } from 'src/common/enums/common.error-handle.enum';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService, private readonly errorHandleService:ErrorHandleService) {}
+
+  @Get('producto/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName:string
+    ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+
+    //return path;
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -20,7 +33,13 @@ export class FilesController {
   }))
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
     
-    if (!file) this.errorHandleService.errorHandle('Make sure that the file is an image', TypeError.BadRequestException)
+    if (!file) this.errorHandleService.errorHandle('Make sure that the file is an image', TypeError.BadRequestException);
+
+    const secureUrl = `${file.filename}`;
+
+    return {
+      secureUrl
+    }
 
   }
  
--- 04-teslo-shop-postgrase/src/files/files.service.ts
+++ 04-teslo-shop-postgrase/src/files/files.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { ErrorHandleService } from 'src/common/common.error-handle.service';
+import { TypeError } from 'src/common/enums/common.error-handle.enum';
 
 @Injectable()
 export class FilesService {
+
+    constructor( private readonly errorHandleService:ErrorHandleService) { }
   
+    getStaticProductImage(imageName:string) {
+        const path = join(__dirname, '../../static/products', imageName);
+
+        if (!existsSync) this.errorHandleService.errorHandle(`Not product found with image: ${imageName}`, TypeError.BadRequestException)
+        
+        return path;
+    }
+
 }
