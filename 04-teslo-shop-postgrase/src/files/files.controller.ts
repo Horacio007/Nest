b0aed3b81cdd806a523299b8772c59cca4280d26
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-12 09:16:42 -0600

    se termina 159 validar tipo de archivo

--- 04-teslo-shop-postgrase/src/files/files.controller.ts
+++ 04-teslo-shop-postgrase/src/files/files.controller.ts
@@ -1,15 +1,22 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter.helper';
+import { ErrorHandleService } from 'src/common/common.error-handle.service';
+import { TypeError } from 'src/common/enums/common.error-handle.enum';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService, private readonly errorHandleService:ErrorHandleService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter
+  }))
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    
+    if (!file) this.errorHandleService.errorHandle('Make sure that the file is an image', TypeError.BadRequestException)
+
   }
  
 }
