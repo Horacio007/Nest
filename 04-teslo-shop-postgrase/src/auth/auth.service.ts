bae008ce623dcd601ea979917c5c4bf7638c6b71
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-29 16:28:07 -0600

    se termina 178 generar json web token

--- 04-teslo-shop-postgrase/src/auth/auth.service.ts
+++ 04-teslo-shop-postgrase/src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import { ErrorHandleService } from 'src/common/common.error-handle.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { TypeError } from 'src/common/enums/common.error-handle.enum';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +15,8 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    private readonly errorHandle: ErrorHandleService
+    private readonly errorHandle: ErrorHandleService,
+    private readonly jwtService:JwtService
   ) { }
 
   async create(createUserDto: CreateUserDto) {
@@ -28,8 +31,10 @@ export class AuthService {
       await this.userRepository.save(user)
       delete user.password;
 
-      // TODO: Retorna json web token
-      return user;
+      return {
+        ...user,
+        token: this.getJwtToken({email: user.email})
+      };
     } catch (error) {
       this.errorHandle.errorHandleDB(error);
     }
@@ -45,8 +50,14 @@ export class AuthService {
 
     if (!user || !bcrypt.compareSync(password, user.password)) this.errorHandle.errorHandle('Credentials not valid, Password or email wrong.', TypeError.UnauthorizedException);
 
-    // TODO: Retorna json web token
-    return user;
+    return {
+      ...user,
+      token: this.getJwtToken({email: user.email})
+    };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 
 }
