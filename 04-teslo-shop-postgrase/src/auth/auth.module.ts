9eabb8f2c1a4be29c4a0fb631b4991e75b498718
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-29 12:38:54 -0600

    se termina 174 nest-authentication passport

--- 04-teslo-shop-postgrase/src/auth/auth.module.ts
+++ 04-teslo-shop-postgrase/src/auth/auth.module.ts
@@ -3,15 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { ErrorHandleService } from 'src/common/common.error-handle.service';
 import { CommonModule } from 'src/common/common.module';
+import { PassportModule, PassportStrategy } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
-    CommonModule
+    CommonModule,
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '5m'
+    //   }
+    // })
   ],
   exports:[TypeOrmModule]
 })
