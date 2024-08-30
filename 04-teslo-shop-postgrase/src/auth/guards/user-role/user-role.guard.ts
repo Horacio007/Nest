99eb742e54e64e28d2d00e3c82c85dfcff50d86f
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-30 15:46:27 -0600

    se termina 184 validar rol de usuario

--- 04-teslo-shop-postgrase/src/auth/guards/user-role/user-role.guard.ts
+++ 04-teslo-shop-postgrase/src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,8 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { use } from 'passport';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -15,8 +17,15 @@ export class UserRoleGuard implements CanActivate {
     
     const validRoles: string[] = this.reflector.get('roles', context.getHandler());
 
-    console.log(validRoles);
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
 
-    return true;
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if( validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(`User ${user.fullName} need a valid role.`)
   }
 }
