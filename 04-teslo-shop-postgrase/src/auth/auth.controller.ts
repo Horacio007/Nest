7bf6328c0b15f7f675e99347f34d2aef588b7081
DesarrolloPLS <desarrollo@plasalle.mx>
2024-08-30 14:15:27 -0600

    se termina 181 custom property decoratorc - GetUser

--- 04-teslo-shop-postgrase/src/auth/auth.controller.ts
+++ 04-teslo-shop-postgrase/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -20,10 +22,16 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @GetUser() user:User
+    // @Req() request:Express.Request
+  ) {
+    // console.log({user});
+    // console.log({user: request.user});
     return {
       ok: true,
-      message: 'Hola mundo private'
+      message: 'Hola mundo private',
+      user: user
     }
   }
 
